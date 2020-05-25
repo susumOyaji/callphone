c9500d344876f8a12a925d951f69a96a7d667264
susumOyaji <sumitomo0210@gmail.com>
2020-05-26 08:56:57 +0900

    up

---
+++ android/app/src/main/kotlin/com/example/callphone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.callphone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Code Snippets',
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new CallPhone(),
+    );
+  }
+}
+
+
+class CallPhone extends StatelessWidget {
+  final String phone = 'tel:+2347012345678';
+
+  _callPhone() async {
+    if (await canLaunch(phone)) {
+      await launch(phone);
+    } else {
+      throw 'Could not Call Phone';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Call Phone from App Example')),
+      body: Center(
+          child: RaisedButton(
+        onPressed: _callPhone,
+        child: Text('Call Phone'),
+        color: Colors.red,
+      )),
+    );
+  }
+}
