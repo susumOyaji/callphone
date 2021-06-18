d66744bea3e385ccafdf523e2e6a455839c82b28
susumOyaji <sumitomo0210@gmail.com>
2021-06-19 08:39:41 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:flt_telephony_info/flt_telephony_info.dart';
 import 'package:flutter/services.dart';
+import 'package:device_info/device_info.dart';
+import 'dart:io' show Platform;
 
 void main() => runApp(new MyApp());
 
@@ -26,6 +28,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    getDeviceInfo();
     //getTelephonyInfo();
 
     Timer.periodic(const Duration(seconds: 10), _timer);
@@ -62,6 +65,65 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void getDeviceInfo() async {
+    DeviceInfoPlugin deviceInfo = new DeviceInfoPlugin();
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      print(_readAndroidBuildData(androidInfo).toString());
+    } else if (Platform.isIOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      print(_readIosDeviceInfo(iosInfo).toString());
+    }
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'androidId': build.androidId
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -79,7 +141,7 @@ class _MyAppState extends State<MyApp> {
               keyboardType: TextInputType.number,
             ),
           ),
-          ElevatedButton(
+          RaisedButton(
             child: Text("Start Call"),
             onPressed: () async {
               FlutterPhoneDirectCaller.callNumber(_numberCtrl.text);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:callphone/mainmain.dart';
+import 'package:callphone/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp1());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
